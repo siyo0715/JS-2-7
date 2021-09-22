232f1059974b6f3b99c0e317f63c2aff705aa8d2
向山　翔 <hms0175km@gmail.com>
2021-09-22 12:01:35 +0900

    [Fnish]2-7

---
+++ app.js
@@ -0,0 +1,12 @@
+// 整数を代入する
+let int1 = 1;
+alert(int1)
+// 負数を代入する
+let int2 = -10;
+alert(int2)
+// 小数点を代入する
+let float1 = 3.14;
+alert(float1)
+// 文字列を代入する
+let str1 = 'JavaScriptを覚えよう';
+alert(str1)
